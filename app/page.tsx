3448911322497fcee7fac8707cda2c2534b0e3e0
cyrus.tang <cyrus.tang@superoverflow.com>
2023-06-15 07:42:02 +0100

    make image into boxes

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,43 @@
 import Image from "next/image";
 
+function ImageContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-[300px] h-[300px]  overflow-clip object-cover object-top rounded-lg shadow-lg hover:scale-95 transition">
+      {children}
+    </div>
+  );
+}
+
 export default function Home() {
+  const photos = [
+    {
+      src: "/01.JPG",
+      width: 900,
+      height: 1600,
+      alt: "front",
+    },
+    {
+      src: "/02.JPG",
+      width: 900,
+      height: 1600,
+      alt: "chanel",
+    },
+    {
+      src: "/03.JPG",
+      width: 900,
+      height: 1600,
+      alt: "side",
+    },
+  ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16 gap-4">
-      <p className="text-lg font-bold text-gray-500">
-        Hello, I'm Tiffany
-      </p>
-      <Image
-        className="border-slate-500 rounded-lg shadow-lg"
-        src="/01.JPG"
-        width={900}
-        height={1600}
-        alt="tiffany"
-      />
-      <Image
-        className="border-slate-500 rounded-lg shadow-lg"
-        src="/02.JPG"
-        width={900}
-        height={1600}
-        alt="tiffany"
-      />
-      <Image
-        className="border-slate-500 rounded-lg shadow-lg"
-        src="/03.JPG"
-        width={900}
-        height={1600}
-        alt="tiffany"
-      />
+      <p className="text-lg font-bold text-zinc-500">Hello👋🏻, I&apos;m Tiffany</p>
+      {photos.map((p) => (
+        <ImageContainer key={p.src}>
+          <Image className="w-full z-0" {...p} alt={p.alt} />
+          <p className="object-top z-10">Lorem Ipsum</p>
+        </ImageContainer>
+      ))}
     </main>
   );
 }
