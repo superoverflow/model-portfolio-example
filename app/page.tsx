bf68aa73cf3fc7a3800d2f82b40e196aaaf94d24
Chiu Yat Tang <tangchiuyat@Chius-MacBook-Air.local>
2023-06-17 11:02:08 +0100

    add filter

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function ImageContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-[300px] h-[300px]  overflow-clip object-cover object-top rounded-lg shadow-lg hover:scale-95 transition">
+    <div className="relative w-[300px] h-[300px] overflow-hidden object-cover object-top rounded-lg shadow-lg">
       {children}
     </div>
   );
@@ -14,28 +14,34 @@ export default function Home() {
       src: "/01.JPG",
       width: 900,
       height: 1600,
-      alt: "front",
+      alt: "Portfolio",
     },
     {
       src: "/02.JPG",
       width: 900,
       height: 1600,
-      alt: "chanel",
+      alt: "Shop",
     },
     {
       src: "/03.JPG",
       width: 900,
       height: 1600,
-      alt: "side",
+      alt: "Contact",
     },
   ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16 gap-4">
-      <p className="text-lg font-bold text-zinc-500">Hello👋🏻, I&apos;m Tiffany</p>
+      <p className="text-lg font-bold text-zinc-500">
+        Hello👋🏻, I&apos;m Tiffany
+      </p>
       {photos.map((p) => (
         <ImageContainer key={p.src}>
-          <Image className="w-full z-0" {...p} alt={p.alt} />
-          <p className="object-top z-10">Lorem Ipsum</p>
+          <Image className="blur brightness-75 w-full z-0" {...p} alt={p.alt} />
+          <div className="absolute inset-0 flex justify-center items-center z-10">
+            <p className="text-2xl p-4 font-bold uppercase text-zinc-100">
+              {p.alt}
+            </p>
+          </div>
         </ImageContainer>
       ))}
     </main>
